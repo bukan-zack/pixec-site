05f93168636de667959e1d722780a80bb3e51af0
zackry <zackry@berrybyte.net>
2023-12-17 10:44:33 +0000

    fix responsiveness

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,27 +14,29 @@ export default function Index() {
                 <h1 className="font-cal-sans text-5xl md:text-7xl text-[#3E362F] text-center tracking-tight">
                     Tailor an unparalleled design.
                 </h1>
-                <div className="mt-2 max-w-2xl mx-auto">
+                <div className="mt-4 max-w-2xl mx-auto">
                     <p className="md:text-xl text-[#5A5D59] text-center">
-                        A design studio that offers all you need — premium website design, branding, advertisement &amp; more.
+                        A design studio that does it all — from professional websites to branding, advertisement &amp; more, to meet all your needs.
                     </p>
                 </div>
                 <div className="w-full mt-6 flex flex-col items-center justify-center md:flex-row md:space-x-4 space-y-3 md:space-y-0">
                     <a href="#" className="w-full md:w-auto flex flex-row justify-center items-center transition-colors ease-in-out duration-500 rounded-full bg-[#3E362F] text-[#e0d1cb] hover:bg-[#4b4139] hover:text-[#e9d9d3] px-12 py-4">
-                        View our work
+                        View Our Work
                     </a>
                     <a href="#" className="w-full md:w-auto flex flex-row justify-center items-center transition-colors ease-in-out duration-500 rounded-full bg-[#e0d1cb] text-[#5A5D59] hover:bg-[#cfc3bd] hover:text-[#3E362F] px-12 py-4">
-                        Have a chat
+                        Contact Us
                     </a>
                 </div>
                 <div className="mt-14">
                     <div className="relative rounded-lg w-full md:w-[55rem] bg-black group overflow-hidden">
-                        <div className="text-white z-[69] text-center absolute inset-0 flex-col items-center justify-center opacity-0 group-hover:opacity-100 flex transition ease-in-out duration-500 delay-100">
+                        <div className="z-[69] absolute inset-0 flex-col items-center justify-center opacity-0 group-hover:opacity-100 flex transition ease-in-out duration-500 delay-100">
                             <div className="max-w-xs md:max-w-4xl">
-                                A picture of BerryByte Landing Page Concept.
+                                <p className="text-center text-[#e0d1cb]">
+                                    A picture of BerryByte Landing Page Concept.
+                                </p>
                             </div>
                         </div>
-                        <img src="https://imadqadri.dev/static/work/berry-landing-concept.png" className="rounded-lg object-fill group-hover:opacity-50 transition ease-in-out duration-500 group-hover:scale-110" />
+                        <img src="https://imadqadri.dev/static/work/berry-landing-concept.png" className="rounded-lg object-fill group-hover:opacity-40 transition ease-in-out duration-500 group-hover:scale-110 group-hover:blur-sm" />
                     </div>
                 </div>
             </div>
